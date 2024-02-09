5f37ba8f386ad3b6d9d678a98d0709b5512388d6
Emirhan Soytaş <emrhns61@hotmail.com>
2024-02-09 20:25:30 +0300

    add lifespan and remove deprecated on_event(startup)

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Session
+from sqlmodel import SQLModel
 
 from database import engine
 from routers import user, session
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app:FastAPI):
     SQLModel.metadata.create_all(engine)
+    yield
 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(user.router)
 app.include_router(session.router)
